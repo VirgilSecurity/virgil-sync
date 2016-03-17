9255dac8ed2895ce8d50e994d3fc05137fc291c4
Dmitry Kushnir <dmykush@gmail.com>
2016-03-17 15:57:00 +0200

    version update

--- Sources/Virgil.Disk/Properties/AssemblyInfo.cs
+++ Sources/Virgil.Disk/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("1.1.27.0")]
-[assembly: AssemblyFileVersion("1.1.27.0")]
+[assembly: AssemblyVersion("1.1.28.0")]
+[assembly: AssemblyFileVersion("1.1.28.0")]
