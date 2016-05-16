35d502772aa22634ba3a252e5cde8dc9eed55a81
dmykush@gmail.com <dmykush@gmail.com>
2016-05-16 16:36:07 +0300

    added separate working mac build

---
+++ Sources/Virgil.Sync.CLI.Monomac/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Virgil.Sync.CLI.Monomac
+{
+	using CommandLine;
+	using Virgil.CLI.Common.Handlers;
+	using Virgil.CLI.Common.Options;
+
+	class MainClass
+	{
+		public static int Main(string[] args)
+		{
+			var parserResult = Parser.Default.ParseArguments<ConfigureOptions, StartOptions>(args);
+
+			var configHandler = new ConfigHandler();
+			var startHandler = new StartHandler();
+
+			return parserResult.MapResult(
+				(ConfigureOptions options) => configHandler.Handle(options),
+				(StartOptions options) => startHandler.Handle(options),
+				errs => 1);
+		}
+	}
+}
